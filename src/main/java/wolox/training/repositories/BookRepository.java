23c97e7f1adf4ee43c528708b5c1948595dafd62
Martin Remis <martin.remis@hotmail.com>
2020-03-12 09:58:22 -0300

    Change extends and make return type optional for BookRepository

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,9 +1,10 @@
 package wolox.training.repositories;
 
+import java.util.Optional;
 import javax.persistence.Id;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import wolox.training.models.Book;
 
-public interface BookRepository extends JpaRepository<Book, Id> {
-  Book findFirstByAuthor(String author);
+public interface BookRepository extends CrudRepository<Book, Id> {
+  Optional<Book> findFirstByAuthor(String author);
 }
