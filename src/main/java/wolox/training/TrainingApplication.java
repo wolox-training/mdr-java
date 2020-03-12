61654376fe36c4761aeece7c89a2dc1da2312b27
Martin Remis <martin.remis@hotmail.com>
2020-03-12 11:28:06 -0300

    Remove unnecessary annotations

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -2,11 +2,7 @@ package wolox.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
-@EntityScan
 @SpringBootApplication
 public class TrainingApplication {
 
