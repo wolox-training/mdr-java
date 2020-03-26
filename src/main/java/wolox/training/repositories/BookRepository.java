bf5403c98c8898e9e50d78f3e89db1972ed0f0f0
martinremis <martin.remis@hotmail.com>
2020-03-26 11:38:58 -0300

    Change implicit queries for JPQL @Query

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -2,7 +2,10 @@ package wolox.training.repositories;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import wolox.training.models.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
@@ -14,5 +17,9 @@ public interface BookRepository extends CrudRepository<Book, Long> {
 
   Optional<Book> findFirstByIsbn(String isbn);
 
-  List<Book> findAllByPublisherAndGenreAndYear(String publisher, String genre, String year);
+  @Query("SELECT b FROM Book b" +
+      " WHERE (:publisher is null OR b.publisher = :publisher)" +
+      " AND (:genre is null OR b.genre = :genre)" +
+      " AND (:year is null OR b.year = :year)")
+  List<Book> findAllByPublisherAndGenreAndYear(@Param("publisher") String publisher, @Param("genre") String genre, @Param("year") String year);
 }
